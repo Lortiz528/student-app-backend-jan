d650bb0f10ffab47537d1c4d9abe66acb2c47786
Lortiz528 <lortiztech@gmail.com>
2023-01-06 14:10:49 -0500

    add env and PORT var

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const app = require('./app');
 
-app.listen(9000, () => console.log('app listening on port 9000'));
+// app.listen(9000, () => console.log('app listening on port 9000'));
+
+const PORT = process.env.PORT || 9000;
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT} `);
+});
